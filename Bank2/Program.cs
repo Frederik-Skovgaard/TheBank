a6701dbee579580a6e53d04c9a15a86593db2212
Frederik Cornelius Skovgaard <fred56b8@elevcampus.dk>
2021-08-11 21:18:52 +0200

    Bank 2 Complet

--- Bank2/Program.cs
+++ Bank2/Program.cs
@@ -9,40 +9,59 @@ namespace Bank2
 {
     class Program
     {
-        Account account;
+        //Class Varibale
+        static Bank bank;
 
-        /// <summary>
-        /// Bank choose Menu
-        /// </summary>
-        /// <param name="args"></param>
+        static bool ShouldLoop = true;
+
         static void Main(string[] args)
         {
             //Call class bank
-            Bank bank = new Bank();
+            bank = new Bank();
 
             //Write name of user
-            Console.WriteLine("Skriv name på bruger: ");
+            Console.Write("Skriv name på bruger: ");
             string name = Console.ReadLine();
 
             //Call class account
             bank.CreateAccount(name);
-            
 
 
-            ////////////////////////////////Start Of Menu//////////////////////////////////////////////////////////////
-                                                                                                                     //
-            //Write bank name                                                                                        //
-            Console.WriteLine(bank.bankName);                                                                        //
-                                                                                                                     //
-            //Write Name & Saldo                                                                                     //
-            Console.WriteLine($"Ny Konto oprettet til {bank.account.Name} med saldoen Kr. {bank.account.Balance}");  //
-                                                                                                                     //
-                                                                                                                     //
-            ////////////////////////////////End Of Menu////////////////////////////////////////////////////////////////
+            //Loop bank menu
+            while (ShouldLoop)
+            {
+                try
+                {
+                    Console.Clear();
 
-            
-            Console.Clear();
+                    //Bank Menu
+                    BankTwoMenu();
 
+                    Console.Clear();
+
+                    if (ShouldLoop)
+                    {
+                        //Press any key to continue
+                        ToContinue();
+                    }
+                } 
+                catch (Exception e)
+                {
+                    Console.Clear();
+
+                    Console.WriteLine(e.Message);
+
+                    ToContinue();
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Bank Menu
+        /// </summary>
+        static void BankTwoMenu()
+        {
             ////////////////////////////////Start Of Menu 2//////////////////////////////////////////////////
                                                                                                            //
             //Write bank name 2                                                                            //
@@ -50,7 +69,7 @@ namespace Bank2
                                                                                                            //
             //Enter a key                                                                                  //
             Console.WriteLine("Vælg venligst...");                                                         //
-                                                                                                           //
+            Console.WriteLine();                                                                           //
             Console.WriteLine("m = Menu");                                                                 //
             Console.WriteLine("k = Konto oprettes");                                                       //
             Console.WriteLine("i = Indsæt");                                                               //
@@ -60,89 +79,126 @@ namespace Bank2
             Console.WriteLine("x = Afslut");                                                               //
                                                                                                            //
             //Read the next key press                                                                      //
-            string readKey = Console.ReadKey().ToString();                                                 //
+            var readKey = Console.ReadKey().Key;                                                           //
                                                                                                            //
-            ////////////////////////////////End Of Menu 2////////////////////////////////////////////////////
+             ////////////////////////////////End Of Menu 2////////////////////////////////////////////////////
 
 
             //Switch for the indiviual key pressed
             switch (readKey)
             {
-                    //Enter Menu
-                case "m":
-
-                    break;
-
-                    //Create account
-                case "k":
-
-                    //Writ account name
-                    Console.WriteLine("Indtast kundens navn: ");
-                    string uName = Console.ReadLine();
-
-                    //Call class Account
-                    bank.CreateAccount(uName);
-
-                    //Show name & balance
-                    Console.WriteLine($"Ny konto oprettet til {uName} med saldoen Kr. {bank.account.Balance}");
-
-                    //Space
-                    Console.WriteLine("");
-
-                    //Continue
-                    Console.WriteLine("Press any key to continue...");
-                    Console.Read();
-
-                    Console.Clear();
-
-                    break;
-
-                    //Deposit money
-                case "i":
-
+                //Enter Menu
+                case ConsoleKey.M:
                     
                     break;
 
-                    //Withdraw money
-                case "h":
+                //Create account
+                case ConsoleKey.K:
+
+                    Console.Clear();
+
+                    CreateAccount();
+                    break;
+
+                //Deposit money
+                case ConsoleKey.I:
+                    Deposit();
+                    break;
+
+                //Withdraw money
+                case ConsoleKey.H:
+                    Withdraw();
+                    break;
+
+                //Show balance
+                case ConsoleKey.S:
+                    Console.WriteLine($"Kontoens saldo: {bank.account.Balance:c}");
+                    break;
+
+                //Show bank
+                case ConsoleKey.B:
 
                     break;
 
-                    //Show balance
-                case "s":
-
-                    break;
-
-                    //Show bank
-                case "b":
-
-                    break;
-
-                    //Exit
-                case "x":
+                //Exit
+                case ConsoleKey.X:
 
                     //Close Console
                     Environment.Exit(0);
 
+                    ShouldLoop = false;
+
                     break;
 
                 default:
-                    break;
+                    throw new ArgumentException("Dette menupunkt eksisterer ikke...");
             }
         }
 
 
-
+        #region Functions
         /// <summary>
-        /// Bank Menu
+        /// Method for creating accounts 
         /// </summary>
-        public void Menu()
-        {          
-            //Call class bank
-            Bank bank = new Bank(); ;
+        static void CreateAccount()
+        {
+            //Writ account name
+            Console.Write("Indtast kundens navn: ");
+            string name = Console.ReadLine();
 
-            
+            //Call class Account
+            bank.CreateAccount(name);
 
+            //Show name & balance
+            Console.WriteLine($"Ny konto oprettet til {name} med saldoen {bank.account.Balance:c}");
         }
+        /// <summary>
+        /// Method for depositing money
+        /// </summary>
+        static void Deposit()
+        {
+            //read info from user
+            Console.Write("Indtast beløb, der sakl indsættets: ");
+            decimal indtast = Convert.ToDecimal(Console.ReadLine());
+
+            //Deposit the insertet money to account balance
+            bank.Deposit(indtast);
+
+            //Show account balance after deposit
+            Console.WriteLine($"Konteons saldo efter indsæt: {bank.account.Balance:c}");
+        }
+        /// <summary>
+        /// Withdraw money from account
+        /// </summary>
+        static void Withdraw()
+        {
+            //Read info from user
+            Console.WriteLine($"Indtast beløb, der skal hæves: ");
+            decimal indtast = Convert.ToDecimal(Console.ReadLine());
+
+            //Withdraw the insertet money to account balance
+            bank.Withdraw(indtast);
+
+            //Show account balance after Withdraw
+            Console.WriteLine($"Kontoens saldo efter hæv: {bank.account.Balance:c}");
+        }
+        #endregion
+        #region Pratical
+        /// <summary>
+        /// Method for ending transaction's
+        /// </summary>
+        static void ToContinue()
+        {
+            //Space
+            Console.WriteLine("");
+
+            //Continue
+            Console.WriteLine("Press any key to continue...");
+            Console.Read();
+
+            //Clear console
+            Console.Clear();
+        }
+        #endregion
     }
 }
