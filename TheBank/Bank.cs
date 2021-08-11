18593f75cd3f5b9f1a7bcac9aadf421e7d788780
Frederik Cornelius Skovgaard <fred56b8@elevcampus.dk>
2021-08-11 10:02:36 +0200

    Initials bank name when called

---
+++ TheBank/Bank.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TheBank
+{
+
+
+    //TODO: Create menue
+    class Bank
+    {
+
+        //Method for getting/initializing bankName
+        public Bank()
+        {
+
+        }
+        public Bank(string bName)
+        {
+            //Give bName a value
+            bName = "*** Welcome To EUC Bank ***";
+
+            //Set bankName value to variable bName
+            this.bankName = bName;
+        }
+
+
+        //Public string bankName set to readonly
+        public string bankName { get; }
+
+    }
+}
