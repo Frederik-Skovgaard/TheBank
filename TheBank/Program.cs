8f3c1e8c8d4d774d5f02eca68229b92960be8d06
Frederik Cornelius Skovgaard <fred56b8@elevcampus.dk>
2021-08-11 09:27:28 +0200

    Add project files.

---
+++ TheBank/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TheBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    //TODO: Create Account
+    class Account
+    {
+
+    }
+
+    class Balance
+    {
+
+    }
+
+    class Deposit
+    {
+
+    }
+
+    class Withdraw
+    {
+
+    }
+
+    class Interests
+    {
+
+    }
+}
