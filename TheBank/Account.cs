2b34a667ffe0a1221bf305626e6db21de96c515c
Frederik Cornelius Skovgaard <fred56b8@elevcampus.dk>
2021-08-11 10:54:56 +0200

    Method for getting name & balance of the account

--- TheBank/Account.cs
+++ TheBank/Account.cs
@@ -10,5 +10,19 @@ namespace TheBank
     class Account
     {
 
+        //Method for getting name and balance
+        public Account(string name)
+        {
+            //Setting variables to Name & Balance
+            this.Balance = 0;
+            this.Name = name;
+        }
+
+
+        //Readonly public string with the name of the account
+        public string Name {get;}
+
+        //Get, Set decimal of account balance
+        public decimal Balance { get; set; }
     }
 }
