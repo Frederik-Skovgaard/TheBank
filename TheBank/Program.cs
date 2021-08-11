d65f521171a56a892571da5f7b7cc6537de8f23a
Frederik Cornelius Skovgaard <fred56b8@elevcampus.dk>
2021-08-11 18:32:05 +0200

    Bank menu & Comments

--- TheBank/Program.cs
+++ TheBank/Program.cs
@@ -8,15 +8,41 @@ namespace TheBank
 {
     class Program
     {
-       static void Main(string[] args)
-        {
-            //Call class bank
-            Bank bankClass = new Bank();
-         
-           
 
-            //Write title to console
-            Console.WriteLine(bankClass.bankName);
-        }
+        /// <summary>
+        /// Bank Menu
+        /// </summary>
+        /// <param name="args"></param>
+       static void Main(string[] args)
+       {
+            //String variable
+            string name = "Katja";
+
+            //Call class bank
+            Bank bankObj = new Bank();
+
+            //Call class account
+            Account acObj = bankObj.CreateAccount(name);
+
+
+            ////////////////////////////////Start Of Menu////////////////////////////////////////////////////
+                                                                                                           //
+            //Write title                                                                                  //
+            Console.WriteLine(bankObj.bankName);                                                           //
+                                                                                                           //
+            //Write Name & Saldo                                                                           //
+            Console.WriteLine($"Ny Konto oprettet til {acObj.Name} med saldoen Kr. {acObj.Balance}");      //
+                                                                                                           //
+            //Write Saldo after deposit                                                                    //
+            Console.WriteLine($"kontoens saldo efter indsæt: Kr. {bankObj.Deposit(500)}");                 //
+                                                                                                           //
+            //Write Saldo after withdraw                                                                   //
+            Console.WriteLine($"Kontoens saldo efter hæv: Kr. {bankObj.Withdraw(300)}");                   //
+                                                                                                           //
+            //Write the saldo                                                                              //
+            Console.WriteLine($"Kontoens saldo: Kr. {acObj.Balance}");                                     //
+                                                                                                           //
+            ////////////////////////////////End Of Menu//////////////////////////////////////////////////////
+       }
     }
 }
